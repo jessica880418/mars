f4ea540e2c6fabce51f4e7be6a610b5e76cbcc94
Jessica <5203086+jessica880418@user.noreply.gitee.com>
2019-08-08 11:37:27 +0800

    add picture of exhibition

--- app/src/main/java/com/example/jessica/mars/MainActivity.kt
+++ app/src/main/java/com/example/jessica/mars/MainActivity.kt
@@ -144,17 +144,17 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-class SwipeHandler(val adapter: PhotoAdapter, dragDirs : Int, swipeDirs : Int) : ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
-    override fun onMove(
-        recyclerView: RecyclerView?,
-        viewHolder: RecyclerView.ViewHolder?,
-        target: RecyclerView.ViewHolder?
-    ): Boolean {
-        return false
-    }
-
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        adapter.removeRow(viewHolder.adapterPosition)
-    }
-
-}
+//class SwipeHandler(val adapter: PhotoAdapter, dragDirs : Int, swipeDirs : Int) : ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+//    override fun onMove(
+//        recyclerView: RecyclerView?,
+//        viewHolder: RecyclerView.ViewHolder?,
+//        target: RecyclerView.ViewHolder?
+//    ): Boolean {
+//        return false
+//    }
+//
+//    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//        adapter.removeRow(viewHolder.adapterPosition)
+//    }
+//
+//}
